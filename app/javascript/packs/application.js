784fb81e8cf4f29226dcffabf8e798070885b001
JPedroValarini <joaopedrovalarini@gmail.com>
2021-02-20 23:01:24 -0300

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// ActiveStorage.start()
+
+import "../src/application.css";
+
+import "./flashMessages";
+
+import "./modal";
